f942793dd5cb060ac3310f506bb64ad6a3b9850c
MJanuszek <monika.januszek.mail@gmail.com>
2022-05-05 17:15:21 +0200

    Added verification to array
    Added verification if new array item is already in array

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
+// 
+// Kurs JS - Ćwiczenia > pseudolosowość
+// 
+
 const button = document.getElementById("draw-btn");
 const messageDiv = document.getElementById("show-message");
+const addButton = document.getElementById("add-new");
+const input = document.getElementById("new-input");
 
-messageDiv.textContent = "Draw youkai (japanese folklore monster)";
+messageDiv.textContent = "Youkai is a creature from japanese folklore";
 
 const youkai = ["Tengu", "Kappa", "Satori", "Yanari", "Yousei", "Bakeneko"];
+let youkaiName = "";
 
 // pseudoprawdopodobieństwo z obieku Math:
 const drawFolkloreMonster = function() {
@@ -11,9 +18,38 @@ const drawFolkloreMonster = function() {
 // losowanie liczby indeksu::::;
 let index = Math.floor(Math.random() * youkai.length);
 console.log(youkai[index]);
-let monster = youkai[index];
-messageDiv.textContent = monster;
+let monsterName = youkai[index];
+messageDiv.textContent = monsterName;
 
 }
 
+// function to add names to youkai array:
+const addNewYoukaiName = function(value){
+    youkaiName = input.value;
+    if (input.value === ""){
+        return
+    } else {
+         // if youkai name is already in array?:::::
+    for (item of youkai){
+        if (item.toLowerCase() === youkaiName.toLowerCase()) {
+            console.log(`this youkai >>${item}<< is already in an array`)
+            return
+        }
+    }
+    }
+   
+
+
+    console.log(input.value);
+    // add to array::::
+    youkai.push(youkaiName);
+    input.value = '';
+    console.log(youkai);
+}
+
+
+// add new youkai name button event and function:::
+addButton.addEventListener("click", addNewYoukaiName);
+
+// draw youkai name button event and function:::::
 button.addEventListener("click", drawFolkloreMonster);
